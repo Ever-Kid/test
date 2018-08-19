533bd2b8d2af32d80d085f93d39a240bbe17c1ab
Ever-Kid <mayiheng6@sina.com>
2018-08-19 15:47:38 -0500

    modified:   hello.c

--- hello.c
+++ hello.c
@@ -1,3 +1,10 @@
-void main(){
-    print("hello mac world");
+#include <stdio.h>
+
+int main(void){
+    int num;
+    printf("Hello Mac World! Give me an integer: \n");
+    scanf("%d",&num);
+    printf("Thanks for your number: %d. \n",num);
+
+    return 0;
 }
